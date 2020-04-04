d48a98e1ac794c54914127c6ca9059ad00004499
Javante Nelson <javantenelson@gmail.com>
2020-04-04 00:35:41 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -2,5 +2,12 @@
 class Anagram
   attr_accessor
   
-  
+   def match(array)
+   word = "listen" 
+  array.select do |wordy| if  wordy.split("").sort == word.split("").sort
+    wordy
+  end
+  end
+    
+  end
 end
